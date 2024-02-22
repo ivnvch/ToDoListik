46e9f037c5dbfc366afd6b99c8b92dbf09f2f924
ivnvch <tarasovets.dmtiv@gmail.com>
2024-02-22 12:33:39 +0300

    feat: add ICommand and IQuery

---
+++ ToDoList.Application/Abstraction/Messaging/ICommandHandler.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ToDoList.Application.Abstraction.Messaging
+{
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse> where TCommand : ICommand<TResponse>
+    {
+    }
+}
---
+++ ToDoList.Application/Abstraction/Messaging/IQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ToDoList.Application.Abstraction.Messaging
+{
+    public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, TResponse> where TQuery : IQuery<TResponse>
+    {
+    }
+}
